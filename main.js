3e7f19e292233d1de4fd9d828dc8ea22cec4d141
Dasha <thisisdasha@gmail.com>
2020-11-23 12:15:27 +0300

    add calendar

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function() {
+    //phone number check
 
     $('#user-phone').inputmask({"mask": "+7-(999)-999-99-99"});
 
@@ -8,7 +9,9 @@ $(document).ready(function() {
         $(this).addClass('input-right-final');
     }
     });
-   
+
+   //user name check
+
     $('#user-name').on('input', function() {
         let re = /^[а-яА-Я ]+$/g;
         if (re.test($(this).val())) {
@@ -33,12 +36,35 @@ $(document).ready(function() {
         }
     })
 
-    $('#date-to-go').inputmask({"mask": "99/99/9999"});
-    $('#date-to-go').on('change', function() {
-        if ($(this)) {
-        $(this).addClass('input-right-final');
-    }
-    })
+    //date check
+
+    //$('#date-to-go').inputmask({"mask": "99/99/9999"});
+    
+    $.datepicker.regional['ru'] = {
+        closeText: 'Закрыть',
+        prevText: 'Предыдущий',
+        nextText: 'Следующий',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Не',
+        dateFormat: 'dd.mm.yy',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+
+    $(function(){
+        $('#date-to-go').datepicker();
+    });
+
+
+    // choosing country
 
     let countries;
     $.ajax({ 
@@ -54,30 +80,23 @@ $(document).ready(function() {
 
 
     $( function() {
-        $( "#country-to-go" ).autocomplete({
+        $( '#country-to-go' ).autocomplete({
             source: countriesArr
         });
     });
 
+    //input validation
 
-
-
-
-    // while ($(window).scrollTop() < $(document).height()) {
-    //     $('.check').each(function() {
-    //         let pos = $(this).offset(),
-    //         wY = $(window).scrollTop();
-    //         //wH = $(window).height(),
-    //         //oH = $(this).outerHeight();
-    //     if (pos.top >= wY) {
-    //         $(this).fadeIn(5000);
-    //     }
-    //     });
-    // }
-    // })
-
-    // $(window).scrollTop() - сколько прокрутил от верха
-    //window.screen.availHeight - высота окна 
+    $(function () { 
+        if ( $('#user-name').val())
+        { if($('#user-phone').val()) {
+            if ($('#country-to-go').val()) {
+                $('.send').attr('disabled') = false;
+            }
+        } // перемотать на телефон ('#user-phone').;
+        } // перемотать на имя
+        }
+     )
 });
 
 
